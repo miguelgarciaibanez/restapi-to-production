0c2219bf105296076f9f3547548bb6059303db42
miguelgarciaibanez <mgarciaibanez@gmail.com>
2022-11-15 12:10:51 +0100

    Adding swagger and scaffolding

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Hello World\!\!\!')
+import { createServer } from "./utils/server";
+
+createServer()
+    .then( server => {
+
+        server.listen(3000, () => {
+            console.log(`Listening on http://localhost:3000`);
+        })
+    })
+    .catch( err => {
+        console.log(`Error: ${err}`);
+    })
