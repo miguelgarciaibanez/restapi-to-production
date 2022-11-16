b8945a52c98f22ce717d18fd5e813a2803cf6f83
miguelgarciaibanez <mgarciaibanez@gmail.com>
2022-11-16 10:14:33 +0100

    Adding logger

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,15 @@
+import bodyParser from "body-parser";
 import express from "express";
 import * as OpenApiValidator from "express-openapi-validator";
 import { Express } from "express-serve-static-core";
+import morgan from "morgan";
+import morganBody from "morgan-body";
 import { connector, summarise } from "swagger-routes-express";
 import YAML from "yamljs";
 
-import * as api from '@exmpl/api/controllers'
+import * as api from '@exmpl/api/controllers';
+import { expressDevLogger } from '@exmpl/utils/express_dev_logger';
+
 
 export async function createServer(): Promise<Express> {
     const yamlSpecFile = './config/openapi.yml';
@@ -24,6 +29,7 @@ export async function createServer(): Promise<Express> {
     server.use(OpenApiValidator.middleware(validatorOptions));
 
     // error customization, if request is invalid
+    
     server.use((err: any, req:express.Request, res:express.Response, next: express.NextFunction)=>{
         res.status(err.status).json({
             error:{
@@ -33,6 +39,12 @@ export async function createServer(): Promise<Express> {
             }
         })
     })
+    
+    server.use(morgan(':method :url :status :response-time ms - :res[content-length]'))
+  
+    morganBody(server)
+  
+    server.use(expressDevLogger)
 
     const connect = connector(api , apiDefinition,{
         onCreateRoute:(method: string, descriptor:any[]) => {
